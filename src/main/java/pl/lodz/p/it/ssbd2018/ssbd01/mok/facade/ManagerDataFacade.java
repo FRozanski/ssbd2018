f266d6ba962bd438ca066a125eaa88ac7e5da0a9
dlange Lange Dominik <203924@edu.p.lodz.pl>
2018-03-26 23:39:36 +0200

    Utworzeenie klas fasady dla MOK

---
+++ src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/mok/facade/ManagerDataFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lodz.p.it.ssbd2018.ssbd01.mok.facade;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import pl.lodz.p.it.ssbd2018.ssbd01.mok.entity.ManagerData;
+
+/**
+ *
+ * @author Dominik Lange
+ */
+@Stateless
+public class ManagerDataFacade extends AbstractFacade<ManagerData> {
+
+    @PersistenceContext(unitName = "ssbd01mokPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ManagerDataFacade() {
+        super(ManagerData.class);
+    }
+    
+}

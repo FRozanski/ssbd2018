a14606689f687e8ba18d2e78173c071a70e252f7
dlange Lange Dominik <203924@edu.p.lodz.pl>
2018-04-01 21:30:37 +0200

    Utworzenie klas fasad dla MOP

---
+++ src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/mop/facade/CategoryFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lodz.p.it.ssbd2018.ssbd01.mop.facade;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import pl.lodz.p.it.ssbd2018.ssbd01.mop.entity.Category;
+
+/**
+ *
+ * @author java
+ */
+@Stateless
+public class CategoryFacade extends AbstractFacade<Category> {
+
+    @PersistenceContext(unitName = "ssbd01mozPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public CategoryFacade() {
+        super(Category.class);
+    }
+    
+}

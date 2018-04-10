547ea59090aa4d631ab17529849e61b3b57c218a
Filip Różański <206275@edu.p.lodz.pl>
2018-04-10 22:38:57 +0200

    wygenerowanie klas encji dla MOK, MOP, MOZ.

---
+++ src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/moz/facade/Order1Facade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lodz.p.it.ssbd2018.ssbd01.moz.facade;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import pl.lodz.p.it.ssbd2018.ssbd01.moz.entity.Order1;
+
+/**
+ *
+ * @author fifi
+ */
+@Stateless
+public class Order1Facade extends AbstractFacade<Order1> implements Order1FacadeLocal {
+
+    @PersistenceContext(unitName = "ssbd01mokPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public Order1Facade() {
+        super(Order1.class);
+    }
+    
+}

ce103963ede13558f2e990ca0cce16f7829491f1
agkan <203899@edu.p.lodz.pl>
2018-04-24 20:01:19 +0200

    utworzone szkielety metod

--- src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/mok/web/AccountController.java
+++ src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/mok/web/AccountController.java
@@ -1,9 +1,31 @@
 package pl.lodz.p.it.ssbd2018.ssbd01.mok.web;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import pl.lodz.p.it.ssbd2018.ssbd01.entities.Account;
+
 /**
  *
  * @author agkan
  */
-public class AccountController {
+public class AccountController implements Serializable{
+    
+    @EJB
+    private MOKEndpointLocal mOKEndpointLocal;
+    
+    private Account account;
+
+    List<Account> listAllAccounts() {
+        return mOKEndpointLocal.getAllAccounts; //TODO: czy tak metoda sie nazywa
+    }
+
+    void getAccountToEdit(Account account) {
+        this.account = mOKEndpointLocal.getAccountToEdit(account); //TODO: czy tak metoda sie nazywa
+    }
+
+    void saveAccount(Account account) {
+        mOKEndpointLocal.saveAllAccounts(); //TODO: czy tak metoda sie nazywa
+    }
     
 }
--- src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/mok/web/ListAccountBean.java
+++ src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/mok/web/ListAccountBean.java
@@ -1,9 +1,31 @@
 package pl.lodz.p.it.ssbd2018.ssbd01.mok.web;
 
+import java.util.List;
+import javax.inject.Inject;
+import pl.lodz.p.it.ssbd2018.ssbd01.entities.Account;
+
 /**
  *
  * @author agkan
  */
 public class ListAccountBean {
+
+    public ListAccountBean() {
+    }
     
+    @Inject
+    private AccountController accountController;
+    
+    public List<Account> getAccounts() {
+        return accountController.listAllAccounts();
+    }
+    
+    public String editAccount(Account account) {
+        accountController.getAccountToEdit(account);
+        return "list-edit";
+    }
+    
+    public void saveAccount(Account account) {
+        accountController.saveAccount(account);
+    }
 }

7d01a5b3c83d19c72a2021aa2c506aec758957e0
michal <203934@edu.p.lodz.pl>
2018-05-21 17:08:54 +0200

    Drobne poprawki.

--- src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/entities/ArchivalPassword.java
+++ src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/entities/ArchivalPassword.java
@@ -84,10 +84,6 @@ public class ArchivalPassword implements Serializable {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getPassword() {
         return password;
     }

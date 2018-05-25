0317f603e2cb5333341c6577d7277ad4358d7f73
michal <203934@edu.p.lodz.pl>
2018-05-25 13:34:43 +0200

    Dodanie brakującej klasy.

---
+++ src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/exceptions/mok/TokenNotFoundException.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lodz.p.it.ssbd2018.ssbd01.exceptions.mok;
+
+/**
+ *
+ * @author michal
+ */
+public class TokenNotFoundException extends VeryficationTokenException {
+
+    public TokenNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TokenNotFoundException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String getCode() {
+        return super.getCode() + ".token_not_found_unique";
+    }
+}

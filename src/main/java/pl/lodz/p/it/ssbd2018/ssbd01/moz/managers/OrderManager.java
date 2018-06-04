33b330be7f7a9f679f401898c09171f0f3a7ab32
agkan <203899@edu.p.lodz.pl>
2018-06-04 22:24:32 +0200

    dodanie RolesAllowed dla MOZ

--- src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/moz/managers/OrderManager.java
+++ src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/moz/managers/OrderManager.java
@@ -8,6 +8,7 @@ package pl.lodz.p.it.ssbd2018.ssbd01.moz.managers;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
@@ -27,51 +28,61 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 public class OrderManager implements OrderManagerLocal{
 
     @Override
+    @RolesAllowed("makeOrder")
     public void makeOrder(OrderProducts orderProducts) {
         throw new NotImplementedException();
     }
 
     @Override
+    @RolesAllowed("makeOrderPayment")
     public void makeOrderPayment(Order1 order) {
         throw new NotImplementedException();
     }
 
     @Override
+    @RolesAllowed("cancelOrder")
     public void cancelOrder(Order1 order) {
         throw new NotImplementedException();
     }
 
     @Override
+    @RolesAllowed("addShippingMethod")
     public void addShippingMethod(ShippingMethod shippingMethod) {
         throw new NotImplementedException();
     }
 
     @Override
+    @RolesAllowed("setShippingMethodActive")
     public void setShippingMethodActive(ShippingMethod shippingMethod, boolean active) {
         throw new NotImplementedException();
     }
 
     @Override
+    @RolesAllowed("setOrderStatus")
     public void setOrderStatus(Order1 order, OrderStatus orderStatus) {
         throw new NotImplementedException();
     }
 
     @Override
+    @RolesAllowed("getAllOrders")
     public List<OrderProducts> getAllOrders() {
         throw new NotImplementedException();
     }
 
     @Override
+    @RolesAllowed("getAllOrdersByAccountAsSeller")
     public List<OrderProducts> getAllOrdersByAccountAsSeller(Account seller) {
         throw new NotImplementedException();
     }
 
     @Override
+    @RolesAllowed("getAllOrdersByAccountAsBuyer")
     public List<OrderProducts> getAllOrdersByAccountAsBuyer(Account buyer) {
         throw new NotImplementedException();
     }
 
     @Override
+    @RolesAllowed("getAllOrdersByDateAndPrice")
     public List<OrderProducts> getAllOrdersByDateAndPrice(Date date, BigDecimal price) {
         throw new NotImplementedException();
     }        

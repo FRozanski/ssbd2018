84816ce5d990e9414676a11e887e3c11618ca5d4
agkan <203899@edu.p.lodz.pl>
2018-06-08 21:33:49 +0200

    dodana weryfikacja, czy użytkownik ma dostęp do danej podstrony

--- front-end/ssbd201801/src/app/mok/common/auth-guard.ts
+++ front-end/ssbd201801/src/app/mok/common/auth-guard.ts
@@ -1,31 +1,48 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
-import { Observable } from "rxjs/Observable";
-import { AccountService } from "./account.service";
-import { AuthUtilService } from "./auth-util.service";
-import { AccountData } from "../model/account-data";
-import { Injectable } from "@angular/core";
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs/Observable';
+import {AuthUtilService} from './auth-util.service';
+import {AccountData} from '../model/account-data';
+import {Injectable} from '@angular/core';
 import {SessionService} from './session.service';
+import {TranslateService} from '@ngx-translate/core';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-    protected identity: AccountData;
+  protected identity: AccountData;
 
-    constructor (protected authUtil : AuthUtilService, protected sessionService: SessionService, ) {
+  userIdentity: AccountData = {};
 
-        this.sessionService.getMyIdentity().subscribe((data: AccountData) => {
-            this.identity = data;
-          }, () => {
-            this.identity = {
-              login: "Guest",
-              roles: ['GUEST']
-            }
-          });
-     }
+  constructor(protected authUtil: AuthUtilService,
+              protected sessionService: SessionService,
+              private translateService: TranslateService) {
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-        let b =  this.authUtil.hasRole(route.data.expectedRoute, this.identity);
-        return true;
-    }
+    this.sessionService.getMyIdentity().subscribe((data: AccountData) => {
+      this.identity = data;
+    }, () => {
+      this.identity = {
+        login: 'Guest',
+        roles: ['GUEST']
+      };
+    });
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.sessionService.getMyIdentity().map(data => {
+      this.userIdentity.roles = data.roles;
+
+      // return this.authUtil.hasRole(route.data.expectedRole, this.userIdentity);
+      console.log('route.data.expectedRole = ' + route.data.expectedRole);
+      console.log('this.userIdentity.roles = ' + this.userIdentity.roles);
+      console.log('1 = ' + this.authUtil.isExpectedRoleInUserRoles(route.data.expectedRole, this.userIdentity.roles));
+      return this.authUtil.isExpectedRoleInUserRoles(route.data.expectedRole, this.userIdentity.roles);
+    }).catch((data) => {
+      this.userIdentity.roles = this.translateService.instant('GUEST');
+      console.log('2 = ' + this.authUtil.isExpectedRoleInUserRoles(route.data.expectedRole, this.userIdentity.roles));
+      return Observable.of(this.authUtil.isExpectedRoleInUserRoles(route.data.expectedRole, this.userIdentity.roles));
+    });
+  }
 
 }
--- front-end/ssbd201801/src/app/mok/common/auth-util.service.ts
+++ front-end/ssbd201801/src/app/mok/common/auth-util.service.ts
@@ -1,15 +1,22 @@
-import { AccountData } from "../model/account-data";
-import { Injectable } from "@angular/core";
+import {AccountData} from '../model/account-data';
+import {Injectable} from '@angular/core';
 
 @Injectable()
 export class AuthUtilService {
 
+  public hasRole(role: string, identity: AccountData): boolean {
+    let hasRole = false;
+    if (identity && identity.roles) {
+      hasRole = identity.roles.indexOf(role) !== -1;
+    }
+    return hasRole;
+  }
 
-    public hasRole(role: string, identity: AccountData): boolean {
-        let hasRole: boolean = false;
-        if(identity && identity.roles) {
-          hasRole = identity.roles.indexOf(role) !== -1;
-        }
-        return hasRole;
-      }
+  public isExpectedRoleInUserRoles(expectedRole: any, roles: string[]): boolean {
+    let is = false;
+    if (expectedRole && roles) {
+      is = roles.indexOf(expectedRole) !== -1;
+    }
+    return is;
+  }
 }

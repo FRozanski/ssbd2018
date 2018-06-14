182d6e6040170a567a5f36a130c599e35119c1a7
Filip Różański <206275@edu.p.lodz.pl>
2018-06-14 15:42:38 +0200

    dodane klasy wyjątków dla moz, shipping method

---
+++ src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/exceptions/moz/ShippingMethodNameNotUniqueException.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lodz.p.it.ssbd2018.ssbd01.exceptions.moz;
+
+/**
+ *
+ * @author Filip
+ */
+public class ShippingMethodNameNotUniqueException extends ShippingMethodException {
+    
+    public ShippingMethodNameNotUniqueException(String message) {
+        super(message);
+    }
+    
+    public ShippingMethodNameNotUniqueException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    @Override
+    public String getCode() {
+        return super.getCode() + ".name_not_unique"; 
+    }
+}

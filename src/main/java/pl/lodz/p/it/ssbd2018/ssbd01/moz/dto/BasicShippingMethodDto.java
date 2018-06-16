825be8a23ad43b7d549651a652e9992f1cf493af
michal <203934@edu.p.lodz.pl>
2018-06-16 11:00:35 +0200

    Dodanie brakującego pliku.

---
+++ src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/moz/dto/BasicShippingMethodDto.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lodz.p.it.ssbd2018.ssbd01.moz.dto;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author michal
+ */
+public class BasicShippingMethodDto {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private boolean active;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the price
+     */
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the active
+     */
+    public boolean isActive() {
+        return active;
+    }
+
+    /**
+     * @param active the active to set
+     */
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}

05535e98ff80a31fe78f4576e33415370c4b0f4b
agkan <203899@edu.p.lodz.pl>
2018-06-17 21:10:24 +0200

    dodanie formularza

--- front-end/ssbd201801/src/app/mop/add-product/add-product.component.ts
+++ front-end/ssbd201801/src/app/mop/add-product/add-product.component.ts
@@ -3,6 +3,9 @@ import {ProductService} from '../common/product.service';
 import {CategoryService} from '../common/category.service';
 import {UnitService} from '../common/unit.service';
 import {LocationService} from '../../mok/common/location.service';
+import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {Location} from '@angular/common';
+import {ErrorStateMatcher} from '@angular/material';
 
 @Component({
   selector: 'app-add-product',
@@ -11,13 +14,70 @@ import {LocationService} from '../../mok/common/location.service';
 })
 export class AddProductComponent implements OnInit {
 
+  form: FormGroup;
+
+  wasFormSent = false;
+
+  formValidationMessage = '';
+
+  categories = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
+  units = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
   constructor(private productService: ProductService,
               private categoryService: CategoryService,
               private unitService: UnitService,
-              private locationService: LocationService) { }
+              private locationService: LocationService,
+              private location: Location) { }
 
   ngOnInit() {
     this.locationService.passRouter('LOCATION.ADD_PRODUCT');
+    this.initializeForm();
+  }
+
+  private initializeForm() {
+    this.form = new FormGroup({
+      name: new FormControl('', [
+        Validators.required
+      ]),
+      category: new FormControl('', [
+        Validators.required
+      ]),
+      description: new FormControl('', [
+        Validators.required
+      ]),
+      price: new FormControl('', [
+        Validators.required
+      ]),
+      qty: new FormControl('', [
+        Validators.required
+      ]),
+      unit: new FormControl('', [
+        Validators.required
+      ]),
+    });
+  }
+
+  isRequiredSatisfied(controlName: string) {
+    return (this.form.get(controlName).errors &&
+      this.form.get(controlName).errors.required) &&
+      ((this.form.get(controlName).dirty || this.form.get(controlName).touched) || this.wasFormSent);
+  }
+
+  onReturnClick() {
+    this.location.back();
+  }
+
+  sendForm() {
+
   }
 
 }

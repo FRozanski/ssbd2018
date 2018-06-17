4940e127d48604b82640181c9ae7ea91d93f1997
agkan <203899@edu.p.lodz.pl>
2018-06-17 21:52:56 +0200

    przekazywanie do selectów wartości z bazy

--- front-end/ssbd201801/src/app/mop/add-product/add-product.component.ts
+++ front-end/ssbd201801/src/app/mop/add-product/add-product.component.ts
@@ -6,6 +6,8 @@ import {LocationService} from '../../mok/common/location.service';
 import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {Location} from '@angular/common';
 import {ErrorStateMatcher} from '@angular/material';
+import {CategoryData} from '../model/category-data';
+import {UnitData} from '../model/unit-data';
 
 @Component({
   selector: 'app-add-product',
@@ -20,17 +22,8 @@ export class AddProductComponent implements OnInit {
 
   formValidationMessage = '';
 
-  categories = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
-  ];
-
-  units = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
-  ];
+  categories: CategoryData[];
+  units: UnitData[];
 
   constructor(private productService: ProductService,
               private categoryService: CategoryService,
@@ -40,7 +33,18 @@ export class AddProductComponent implements OnInit {
 
   ngOnInit() {
     this.locationService.passRouter('LOCATION.ADD_PRODUCT');
+
     this.initializeForm();
+
+    this.categoryService.getAllCategories().subscribe((categories) => {
+      this.categories = categories.map(c => Object.assign({}, c));
+      console.log('categories[0] = ' + categories[0].categoryName);
+    });
+
+    this.unitService.getAllUnits().subscribe((units) => {
+      this.units = units.map(u => Object.assign({}, u));
+      console.log('units[0] = ' + units[0].unitName);
+    });
   }
 
   private initializeForm() {

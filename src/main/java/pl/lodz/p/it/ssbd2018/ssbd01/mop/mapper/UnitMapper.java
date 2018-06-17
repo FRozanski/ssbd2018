3c9eecb3d1fc22d6c8e8553218c703120e3d3419
agkan <203899@edu.p.lodz.pl>
2018-06-17 16:20:08 +0200

    mapper jednostek

---
+++ src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/mop/mapper/UnitMapper.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lodz.p.it.ssbd2018.ssbd01.mop.mapper;
+
+import java.util.List;
+import org.mapstruct.Mapper;
+import pl.lodz.p.it.ssbd2018.ssbd01.entities.Unit;
+import pl.lodz.p.it.ssbd2018.ssbd01.mop.dto.BasicUnitDto;
+
+/**
+ *
+ * @author agkan
+ */
+@Mapper
+public interface UnitMapper {
+    
+    List<BasicUnitDto> unitsToDto(List<Unit> units);
+}

f783d6255d8f9ffc74b05800f8b1373824afa80d
Filip Różański <206275@edu.p.lodz.pl>
2018-06-19 00:08:45 +0200

    dodanie metody wysyłki, działa sztosik : )

--- front-end/ssbd201801/src/app/moz/add-shipping-method/add-shipping-method.component.ts
+++ front-end/ssbd201801/src/app/moz/add-shipping-method/add-shipping-method.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Location} from '@angular/common';
+import {MatDialog, MatDialogRef} from '@angular/material';
+import {LocationService} from '../../mok/common/location.service';
+import { ShippingMethodData } from '../model/shippingMethod-data';
+import { ConfirmDialogComponent } from '../../shared/confirm-dialog/confirm-dialog.component';
+import { ShippingMethodService } from '../common/shipping-method.service';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-add-shipping-method',
@@ -15,13 +22,48 @@ export class AddShippingMethodComponent implements OnInit {
 
   formValidationMessage = '';
 
-  constructor(private location: Location) { }
+  shippingMethod: ShippingMethodData = {};
+
+  dialogRef: MatDialogRef<ConfirmDialogComponent>;
+
+  constructor(private shippingMethodService: ShippingMethodService,
+    private location: Location,
+    public dialog: MatDialog,
+    private locationService: LocationService,
+    private router: Router,
+    private translateService: TranslateService) { }
 
   ngOnInit() {
+    this.locationService.passRouter('LOCATION.ADD_SHIPPING_METHOD_PAGE');
+
     this.initializeForm();
   }
 
   sendForm() {
+    this.wasFormSent = true;
+
+    if (this.form.valid) {
+
+      this.dialogRef = this.dialog.open(ConfirmDialogComponent, {
+        disableClose: false
+      });
+
+      this.dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          this.shippingMethod = <ShippingMethodData>this.form.value;
+          this.shippingMethod.confirm = true;
+          this.shippingMethodService.addShippingMethod(this.shippingMethod).subscribe(() => {
+            alert(this.translateService.instant('SUCCESS.ADD_SHIPPING_METHOD'));
+              this.router.navigate(['/main']);
+          },
+          (errorResponse) => {
+            console.log(errorResponse.error.message);
+              this.formValidationMessage = this.translateService.instant(errorResponse.error.message);
+            });
+        }
+        this.dialogRef = null;
+      });
+    }
   }
 
   onReturnClick() {
--- front-end/ssbd201801/src/app/moz/common/shipping-method.service.ts
+++ front-end/ssbd201801/src/app/moz/common/shipping-method.service.ts
@@ -3,6 +3,7 @@ import {Observable} from 'rxjs/Observable';
 import {Order} from '../model/order';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
+import { ShippingMethodData } from '../model/shippingMethod-data';
 
 @Injectable()
 export class ShippingMethodService {
@@ -15,4 +16,7 @@ export class ShippingMethodService {
     return this.httpClient.get<Order[]>(this.uri);
   }
 
+  addShippingMethod(shipingMethod: ShippingMethodData) {
+    return this.httpClient.post<ShippingMethodData>(this.uri + '/addShippingMethod', shipingMethod);
+  }
 }

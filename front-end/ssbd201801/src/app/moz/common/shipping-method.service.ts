07418f911a4a9438275b0ff4b1f9520179c4245b
michal <203934@edu.p.lodz.pl>
2018-06-20 14:45:50 +0200

    Poprawka błędu.

--- front-end/ssbd201801/src/app/moz/common/shipping-method.service.ts
+++ front-end/ssbd201801/src/app/moz/common/shipping-method.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
-import { ShippingMethodData } from '../model/shippingMethod-data';
 import {ShippingMethod} from '../model/shipping-method';
 
 @Injectable()
@@ -28,7 +27,7 @@ export class ShippingMethodService {
     return this.httpClient.put(this.uri + '/deactivate', null, {params});
   }
 
-  addShippingMethod(shipingMethod: ShippingMethodData) {
-    return this.httpClient.post<ShippingMethodData>(this.uri + '/addShippingMethod', shipingMethod);
+  addShippingMethod(shipingMethod: ShippingMethod) {
+    return this.httpClient.post<ShippingMethod>(this.uri + '/addShippingMethod', shipingMethod);
   }
 }

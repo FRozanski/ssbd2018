3bfd40eefec81e65a60ad3a3899dacaa2cc3e9a2
Filip Różański <206275@edu.p.lodz.pl>
2018-06-20 23:22:36 +0200

    aktywacja, deaktywacja kategorii produktów

--- src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/mop/facades/CategoryFacade.java
+++ src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/mop/facades/CategoryFacade.java
@@ -9,8 +9,13 @@ import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.OptimisticLockException;
 import javax.persistence.PersistenceContext;
+import javax.validation.ConstraintViolationException;
 import pl.lodz.p.it.ssbd2018.ssbd01.entities.Category;
+import pl.lodz.p.it.ssbd2018.ssbd01.exceptions.AppBaseException;
+import pl.lodz.p.it.ssbd2018.ssbd01.exceptions.mop.CategoryException;
+import pl.lodz.p.it.ssbd2018.ssbd01.exceptions.mop.CategoryOptimisticException;
 import pl.lodz.p.it.ssbd2018.ssbd01.shared_facades.AbstractFacadeCreateUpdate;
 
 /**
@@ -37,4 +42,16 @@ public class CategoryFacade extends AbstractFacadeCreateUpdate<Category> impleme
     public List<Category> findAll() {
         return super.findAll();
     }    
+    
+    @Override
+    @RolesAllowed("editCategory")
+    public void edit(Category category) throws AppBaseException {
+        try {
+            super.edit(category);
+        } catch (OptimisticLockException oe) {
+            throw new CategoryOptimisticException("category_optimistic_exception");
+        } catch (ConstraintViolationException ex) {
+            throw new CategoryException("constraint_violation");
+        }
+    }
 }

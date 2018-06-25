c47de88ffbaa081284bed8cda91c2c6716f9dca3
michal <203934@edu.p.lodz.pl>
2018-06-26 01:19:12 +0200

    Wprowadzenie poprawek.

--- src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/mok/rest/SessionRestController.java
+++ src/main/java/pl/lodz/p/it/ssbd2018/ssbd01/mok/rest/SessionRestController.java
@@ -5,8 +5,12 @@
  */
 package pl.lodz.p.it.ssbd2018.ssbd01.mok.rest;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -17,6 +21,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.json.simple.JSONObject;
 import pl.lodz.p.it.ssbd2018.ssbd01.exceptions.AppBaseException;
+import pl.lodz.p.it.ssbd2018.ssbd01.exceptions.InternalException;
 import pl.lodz.p.it.ssbd2018.ssbd01.exceptions.WebErrorInfo;
 import pl.lodz.p.it.ssbd2018.ssbd01.exceptions.web.UserAlreadyLogoutException;
 
@@ -26,6 +31,8 @@ import pl.lodz.p.it.ssbd2018.ssbd01.exceptions.web.UserAlreadyLogoutException;
  */
 @Path("session")
 public class SessionRestController {
+    
+    private static final String CONFIG_FILE = "roles.properties";
 
     /**
      * Metoda udostępniająca endpoint REST w celu pozyskania loginu w danej sesji.
@@ -73,6 +80,11 @@ public class SessionRestController {
         } catch (UserAlreadyLogoutException ex) {
             json.put("roles", "GUEST");
             json.put("login", "GUEST");      
+        } catch (AppBaseException ex) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(new WebErrorInfo("400", ex.getCode()))
+                    .type(MediaType.APPLICATION_JSON)
+                    .build();
         }
         return Response.status(Response.Status.OK)
                     .entity(json)
@@ -98,19 +110,30 @@ public class SessionRestController {
      * @param servletRequest
      * @return listę ról w danej sesji. 
      */
-    private List<String> getUserRoles(HttpServletRequest servletRequest) {
+    private List<String> getUserRoles(HttpServletRequest servletRequest) throws AppBaseException {
         List<String> levels = new ArrayList<>();
-        if (servletRequest.isUserInRole("ADMIN")) {
-            levels.add("ADMIN");
+        Properties properties = new Properties();
+        try {
+            InputStream inputStream = this.getClass().getClassLoader()
+                .getResourceAsStream(CONFIG_FILE);
+            System.out.println("InputStream is: " + inputStream);
+            properties.load(inputStream);   
+        } catch (FileNotFoundException ex) {
+            throw new InternalException("file_not_find");
+        } catch (IOException ex) {
+            throw new InternalException("io_exception");
         }
-        if (servletRequest.isUserInRole("MANAGER")) {
-            levels.add("MANAGER");
+        if (servletRequest.isUserInRole(properties.getProperty("role.admin"))) {
+            levels.add(properties.getProperty("role.admin"));
         }
-        if (servletRequest.isUserInRole("USER")) {
-            levels.add("USER");
+        if (servletRequest.isUserInRole(properties.getProperty("role.manager"))) {
+            levels.add(properties.getProperty("role.manager"));
         }
-        if (servletRequest.isUserInRole("VIRTUAL")) {
-            levels.add("VIRTUAL");
+        if (servletRequest.isUserInRole(properties.getProperty("role.user"))) {
+            levels.add(properties.getProperty("role.user"));
+        }
+        if (servletRequest.isUserInRole(properties.getProperty("role.virtual"))) {
+            levels.add(properties.getProperty("role.virtual"));
         }
         return levels;
     }
